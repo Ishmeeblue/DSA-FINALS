a9d6159b70dbaf034f59fab656bba842c6dacebf
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-25 01:05:46 +0800

    feat:

--- frontend/src/context/shopcontext.jsx
+++ frontend/src/context/shopcontext.jsx
@@ -12,7 +12,7 @@ const ShopContextProvider = (props) => {
     const [cartItems,setCartItems] = useState({});
     const navigate = useNavigate();
     const [token,setToken] = useState('');
-    const backendUrl = "http://localhost:4000"; 
+    const backendUrl = "dsa-finals-backend.vercel.app"; 
 
     const addToCart = async (itemId, shades) => {
     if (!shades) {
