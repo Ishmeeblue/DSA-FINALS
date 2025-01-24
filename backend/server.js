61bd878151a047f899b6c404067e67a36ffa76c3
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-24 23:04:54 +0800

    feat: :sparkles:

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mondodb.js'
-import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
 
 
 const app= express()
 const port= process.env.PORT || 4000
 connectDB()
-connectCloudinary()
 
 app.use(express.json())
 app.use(cors())
