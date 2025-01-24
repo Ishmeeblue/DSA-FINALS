b57d7968edba1b78d47b79a26267d60c40b55841
Ishmeeblue <165567488+Ishmeeblue@users.noreply.github.com>
2025-01-24 23:44:11 +0800

    feat: :sparkles:

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
 import connectDB from './config/mondodb.js'
-import userRouter from './routes/userRoute.js'
+import userRouter from './routes/userRoute.js';
 
+const app = express();
+const port = process.env.PORT || 4000;
 
-const app= express()
-const port= process.env.PORT || 4000
-connectDB()
+connectDB();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/user',userRouter)
+app.use('/api/user', userRouter);
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
-})
+app.get('/', (req, res) => {
+    res.send("API Working");
+});
 
-app.listen(port, ()=> console.log('Server started on PORT : '+ port))
+export default app;
